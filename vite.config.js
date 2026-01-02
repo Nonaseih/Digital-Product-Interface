491d5515cac0c13122235172f3cc0ef2deede840
Anon <Fortunestephen720@gmail.com>
2026-01-02 02:46:50 +0100

    Add gh-pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Digital-Product-Interface/',
 })
